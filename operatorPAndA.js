73122f32a11e5843557373c62488853954d41639
JoshuaMaddox <joshua@cinderhousecreative.com>
2016-08-13 23:34:05 +0800

    functional operators that demonstrate Operator Precedence and Associativity

---
+++ operatorPAndA.js
@@ -0,0 +1,23 @@
+//Operator Precedence and Associativity
+var a = 2, b = 3, c = 4;
+
+a += b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var d = 2, e = 3, f = 4;
+
+d = e = f;
+
+console.log(d);
+console.log(e);
+console.log(f);
+
+
+
+
+
+
+
