8918431a9e13d90c817432cccb9fa383c33bb90f
JoshuaMaddox <joshua@cinderhousecreative.com>
2016-08-14 13:52:02 +0800

    simple prototypical methods that show how to parse and convert JSON to and from Object literals

---
+++ jsonConversion.js
@@ -0,0 +1,18 @@
+//Converting JSON
+
+//Create an ObjectLiteral
+var objectLiteral = {
+	firstName: 'Mary',
+	isAProgrammer: true
+}
+
+//Log the Object Literaly into a JSON string
+console.log(JSON.stringify(objectLiteral));
+
+//Sets the variable jsonValue to a JavaScript Object using JSON.parse
+var jsonValue = JSON.parse({"firstName":"Mary","isAProgrammer":true});
+
+console.log(jsonValue);
+
+
+
