4ee69564464c1aa8e98b0ea7fe67142c39db2073
JoshuaMaddox <joshua@cinderhousecreative.com>
2016-08-21 16:31:50 +0800

    added the ROT13 version of ceasers cypher

--- CeasersCypher.js
+++ CeasersCypher.js
@@ -20,7 +20,26 @@ function CeasarCipher()
 		}	
 }
 
-function try(offSetStr)
-{
 
+// the ROT13 Version:
+
+
+function rot13(str) {
+	var tempVar = '';
+	var tempVarConv = '';
+	var newStr = '';
+	for(var i = 0; i < str.length; i++){
+		var tempVar = str[i].charCodeAt(0);
+		if(tempVar < 65 || tempVar > 90){
+			newStr += String.fromCharCode(tempVar);
+		} else if(tempVar > 64 && tempVar < 78) {
+			tempVar = tempVar + 13;
+			newStr += String.fromCharCode(tempVar);
+		} else if (tempVar > 77 && tempVar < 91) {
+			tempVar = tempVar - 13;
+			newStr += String.fromCharCode(tempVar);
+		}
+	}
+	str = newStr;
+	return str;
 }
